fce2057bb52633b3a942428a111b3e2062df2b4c
Harsh2426 <harshpatel9811838836@gmail.com>
2023-11-27 18:22:46 +0530

    adding files

--- 01_PATTERN/striver/22.cpp
+++ 01_PATTERN/striver/22.cpp
@@ -7,7 +7,19 @@
  4 3 2 1 2 3 4
  4 3 2 2 2 3 4
  4 3 3 3 3 3 4
- 4 4 4 4 4 4 4 
+ 4 4 4 4 4 4 4
+
+ AFTER SUBTRACTING EACH ELEMENT BY N.
+
+ 0 0 0 0 0 0 0
+ 0 1 1 1 1 1 0
+ 0 1 2 2 2 1 0
+ 0 1 2 3 2 1 0
+ 0 1 2 2 2 1 0
+ 0 1 1 1 1 1 0
+ 0 0 0 0 0 0 0
+
+
 
 */
 
@@ -16,12 +28,17 @@ using namespace std;
 
 void helper(int n)
 {
-    for(int i=0;i<2*n-1;i++){
-        for(int j=0;j<2*n-1;j++){
-            if(i==0 or i)
-            cout<<n-i<<" ";
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int bottom = (2 * n - 2) - i;
+            int right = (2 * n - 2) - j;
+            cout << n - min({top, left, right, bottom}) << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
